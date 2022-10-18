268e6b2c723c71acb1cdd04cd7f9c6bed4b35a36
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-18 22:26:35 +0530

    first commit

---
+++ src/com/bridgelaz/AccountProblem2.java
@@ -0,0 +1,31 @@
+package com.bridgelaz;
+
+import java.util.Scanner;
+
+public class AccountProblem2 {
+    long balance = 9111;
+    public long debitFromAccount(int amount){
+        if(balance>=amount){
+            balance = balance-amount;
+            System.out.println("Money debited successfully.");
+            return balance;
+        }else {
+            System.out.println("Debit amount is exceeding account balance. ");
+            return balance;
+        }
+
+    }
+}
+class TestMethodDebit{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AccountProblem2 account = new AccountProblem2();
+        int amount;
+        do{
+            System.out.println("Enter the debit amount : ");
+            amount = scanner.nextInt();
+            long balance = account.debitFromAccount(amount);
+            System.out.println("Your account balance is : " + balance);
+        }while (amount>=account.balance);
+    }
+}
---
+++ src/com/bridgelaz/Problem4DeckOfCards.java
@@ -0,0 +1,43 @@
+package com.bridgelaz;
+
+import java.util.Random;
+
+public class Problem4DeckOfCards {
+    Random random = new Random();
+    String[] suit = {"Clubs","Diamonds","Hearts","Spades"};
+    String[] rank ={"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+    public void distributeCards(){
+        int suitIndex,rankIndex;
+        String[][] result = new String[9][1];
+        for (int i = 0;i<result.length;i++){
+            for (int j = 0;j<result[i].length;j++) {
+                suitIndex = random.nextInt(4);
+                rankIndex = random.nextInt(13);
+                result[i][j] = suit[suitIndex]+" "+rank[rankIndex];
+            }
+        }
+        for (int k=0,i = 0;i<result.length;i++){
+            for (int j = 0;j<result[i].length;j++) {
+                k++;
+                System.out.print("Card "+k+" : ");
+                System.out.println( result[i][j]);
+            }
+        }
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Problem4DeckOfCards player1 = new Problem4DeckOfCards();
+        Problem4DeckOfCards player2 = new Problem4DeckOfCards();
+        Problem4DeckOfCards player3 = new Problem4DeckOfCards();
+        Problem4DeckOfCards player4 = new Problem4DeckOfCards();
+        System.out.println("Cards for player 1");
+        player1.distributeCards();
+        System.out.println("Cards for player 2");
+        player2.distributeCards();
+        System.out.println("Cards for player 3");
+        player3.distributeCards();
+        System.out.println("Cards for player 4");
+        player4.distributeCards();
+    }
+}
